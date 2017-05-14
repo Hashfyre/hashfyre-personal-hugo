b41f7d8f0caf17583645edef376875d48837ab82
Joy Bhattacherjee <joy.bhattacherjee@razorpay.com>
2017-05-14 14:48:47 +0530

    rebuilding site Sun May 14 14:48:47 IST 2017

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+PWD=`pwd`
+# cleanup
+rm -rf public/
+
+# create public
+mkdir public
 
 # Build the project.
 #hugo # if using a theme, replace by `hugo -t <yourtheme>`
@@ -24,3 +30,27 @@ git push --force origin master
 
 # Come Back
 cd ..
+
+## Main site update
+# cleanup main-site
+cd ../hashfyre.github.io/
+rm -rf ./*
+
+# copy
+cp -R ../hashfyre-personal-hugo/public/* ./
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push --force origin master
+
+# come back
+cd ${PWD}
